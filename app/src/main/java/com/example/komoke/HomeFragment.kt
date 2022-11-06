01c6f3879d55b700b4d8bc27c6f4c1548d2eaff5
nandajuant <nandajuant@gmail.com>
2022-11-06 22:44:17 +0700

    UGD89_Kelompok 10

--- app/src/main/java/com/example/komoke/HomeFragment.kt
+++ app/src/main/java/com/example/komoke/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.example.komoke
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.komoke.MainActivity
-
+import com.example.komoke.maps.MapsActivity
+import kotlinx.android.synthetic.main.fragment_home.*
+import com.example.komoke.HomeActivity
 
 class HomeFragment : Fragment() {
 
@@ -16,7 +19,21 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+//        btnHotel.setOnClickListener(){
+//            val moveMaps = Intent(context, MapsActivity::class.java)
+//            startActivity(moveMaps)
+//        }
     }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        btnHotel.setOnClickListener(){
+//            val moveMaps = Intent(context, MapsActivity::class.java)
+//            startActivity(moveMaps)
+//        }
+//    }
 
 
 }
